aff16cdc53fedb31fd6025170a9d4e91c7baf6f4
sudan bhusal <sudanbhusal20215@gmail.com>
2020-10-22 23:10:29 -0700

    modified everything

--- quiz.js
+++ quiz.js
@@ -1,20 +1,5 @@
-window.onload = confirm("Do you want to be a millionaire");
-function startTimer(){
-    var counter = 10;
-    setInterval(function() {
-      counter--;
-      if (counter >= 0) {
-        span = document.getElementById("count");
-        span.innerHTML = counter;
-      }
-      if (counter === 0) {
-          alert('sorry, out of time');
-          clearInterval(counter);
-      }
-    }, 1000);
-  }
-  function start()
-  {
-      document.getElementById("count").style="color:green;";
-      startTimer();
-  };
+function submitForm(e){
+  e.preventDefault();
+  let name=document.forms["welcomeF"]["name"].value;
+  console.log(name);
+}
