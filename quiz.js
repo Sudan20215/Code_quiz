8f9e6499a3c84912e766607b00db327d5056c213
sudan bhusal <sudanbhusal20215@gmail.com>
2020-10-23 08:42:07 -0700

    question and option added

--- quiz.js
+++ quiz.js
@@ -1,15 +1,55 @@
 window.onload=function(){
-  display();
+  display(0);
 }
-let question=[
+let questionl=[
   {
     id:1,
-    question: "my name",
-    answer:"sudan",
+    question: "Who invented computer?",
+    answer:"Charles Babbage",
     option:[
-      "asa",
-      "afa",
-      "fsdfs"
+      "Thomas Edison",
+      "Albert einstein",
+      "None of the above"
+    ]
+  },
+{
+    id:2,
+    question: "When did first program founded?",
+    answer:"1989",
+    option:[
+      "1982",
+      "1980",
+      "1780"
+    ]
+  },
+{
+    id:3,
+    question: "What is the name of first program?",
+    answer:"High-level language",
+    option:[
+      "Machine level language",
+      "javascript",
+      "css"
+    ]
+  },
+{
+    id:4,
+    question: "What is the Capital of Usa?",
+    answer:"D.c",
+    option:[
+      "NY",
+      "Cali",
+      "Virginia"
+    ]
+  },
+{
+    id:5,
+    question: "Who invented Java?",
+    answer:"James Gosling",
+    option:[
+      "Bill Gates",
+      "Steve Jobs",
+      "Daphne koller"
     ]
   }
 ]
@@ -23,10 +63,19 @@ function submitForm(e){
 let question_count=0;
 function nextf(){
   question_count++;
+  display(question_count);
   console.log(question_count);
 
 }
 function display(count){
-  let question =document.querySelector("#questions1");
-  question.innerHTML= "<h3>"+questions1[0].questions1+"</h3>";
+  let question =document.getElementById("questions1");
+  question.innerHTML=`<h3>${questionl[count].question}</h3>
+  <ul class="optionlist">
+                    <li class="option">${questionl[count].option[0]}</li>
+                    <li class="option">${questionl[count].option[1]}</li>
+                    <li class="option">${questionl[count].option[2]}</li>
+                    <li class="option">${questionl[count].option[3]}</li>
+                </ul>
+  `;
+
 }
