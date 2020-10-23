8722029e6c99475016b0eeeca24fee3eb4bc9624
sudan bhusal <sudanbhusal20215@gmail.com>
2020-10-23 01:06:20 -0700

    first question added

--- quiz.js
+++ quiz.js
@@ -1,7 +1,32 @@
+window.onload=function(){
+  display();
+}
+let question=[
+  {
+    id:1,
+    question: "my name",
+    answer:"sudan",
+    option:[
+      "asa",
+      "afa",
+      "fsdfs"
+    ]
+  }
+]
 function submitForm(e){
   e.preventDefault();
   let name=document.forms["welcomeF"]["name"].value;
   console.log(name);
   location.href="quiz1.html"
   sessionStorage.setItem("name",name);
+}
+let question_count=0;
+function nextf(){
+  question_count++;
+  console.log(question_count);
+
+}
+function display(count){
+  let question =document.querySelector("#questions1");
+  question.innerHTML= "<h3>"+questions1[0].questions1+"</h3>";
 }
