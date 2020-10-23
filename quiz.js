e09a157d2e6a904838092fefe0258ce2b4ef8602
sudan bhusal <sudanbhusal20215@gmail.com>
2020-10-23 00:33:48 -0700

    styling done

--- quiz.js
+++ quiz.js
@@ -2,4 +2,6 @@ function submitForm(e){
   e.preventDefault();
   let name=document.forms["welcomeF"]["name"].value;
   console.log(name);
+  location.href="quiz1.html"
+  sessionStorage.setItem("name",name);
 }
